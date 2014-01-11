8736d9e061c2256f59333582c66a7f82a890e9da
John Bartholomew <jpa.bartholomew@gmail.com>
2014-01-11 15:32:50 +0000

    fix unused-parameter warning in nfai_quoted_char in NDEBUG build

--- nfa.c
+++ nfa.c
@@ -198,8 +198,11 @@ NFA_INTERNAL const char *NFAI_ERROR_DESC[] = {
 #ifndef NFA_NO_STDIO
 NFA_INTERNAL const char *nfai_quoted_char(int c, char *buf, size_t bufsize) {
    NFAI_ASSERT(c >= 0 && c <= UINT8_MAX);
-   /* max length is for '\xFF', which is 7 bytes (including null terminator) */
+   /* max length is for '\xFF', which is 7 bytes (including null terminator)
+    * this is an assert because nfai_quoted_char is internal, ie, if bufsize is < 7
+    * then that's a bug in the code in this file somewhere */
    NFAI_ASSERT(bufsize >= 7);
+   (void)bufsize; /* prevent unused parameter warning for NDEBUG builds */
    if (c >= 32 && c < 127) {
       sprintf(buf, "'%c'", (char)c);
    } else {
