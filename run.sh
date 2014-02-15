d47466690802e570fb5f1d25ff0b3cd56207e4b5
John Bartholomew <jpa.bartholomew@gmail.com>
2014-02-15 18:08:35 +0000

    fix bug in run.sh (incorrect quoting of arguments)

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gcc -std=c89 -pedantic -Wall -Wextra -O0 -g -o example example.c && exec ./example $*
+gcc -std=c89 -pedantic -Wall -Wextra -O0 -g -o example example.c && exec ./example "$@"
