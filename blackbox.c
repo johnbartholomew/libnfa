5f64b0626761afcc80e6b5893f523826f54b57df
John Bartholomew <jpa.bartholomew@gmail.com>
2014-04-23 04:06:23 +0100

    blackbox: print summary stats and don't print every PASS

--- blackbox.c
+++ blackbox.c
@@ -29,7 +29,8 @@ static int match_nfa(const Nfa *nfa, const char *string) {
    NfaMachine exec;
    int result;
 
-   if (!nfa) { return 0; }
+   assert(nfa);
+   assert(string);
 
    nfa_exec_init_pool(&exec, nfa, 0, EXEC_POOL, sizeof(EXEC_POOL));
    result = nfa_exec_match_string(&exec, string, -1);
@@ -37,16 +38,18 @@ static int match_nfa(const Nfa *nfa, const char *string) {
 
    if (result < 0) {
       fprintf(stderr, "bug: error while executing NFA on input '%s' (%s)\n", string, nfa_error_string(result));
-      return 0;
+      return -1;
    }
 
-   return !!result;
+   return result;
 }
 
 static void run_tests(FILE *fl) {
    char buf[512];
    char pattern[512];
    Nfa *nfa = NULL;
+   int pattern_count = 0, test_count = 0, fail_count = 0, skip_count = 0;
+
    while (1) {
       size_t len;
       char *line = fgets(buf, sizeof(buf), fl);
@@ -72,6 +75,8 @@ static void run_tests(FILE *fl) {
          free(nfa);
          strcpy(pattern, line + 2);
          nfa = build_nfa(line + 2);
+         ++pattern_count;
+         if (!nfa) { ++skip_count; }
          /* nfa_print_machine(nfa, stdout); */
       } else {
          int matched, expected;
@@ -82,15 +87,24 @@ static void run_tests(FILE *fl) {
             continue;
          }
 
-         matched = match_nfa(nfa, line + 2);
-         if (!!matched == !!expected) {
-            fprintf(stdout, " ok   (/%s/ %s '%s')\n", pattern, (matched ? "~=" : "~!"), line + 2);
-         } else {
-            fprintf(stdout, "FAIL  (/%s/ %s '%s')\n", pattern, (matched ? "~=" : "~!"), line + 2);
+         if (nfa) {
+            ++test_count;
+            matched = match_nfa(nfa, line + 2);
+            if (matched < 0) {
+               ++fail_count;
+            } else if (matched == expected) {
+               /* fprintf(stdout, " ok   (/%s/ %s '%s')\n", pattern, (matched ? "~=" : "~!"), line + 2); */
+            } else {
+               ++fail_count;
+               fprintf(stdout, "FAIL  (/%s/ %s '%s')\n", pattern, (matched ? "~=" : "~!"), line + 2);
+            }
          }
       }
    }
    free(nfa);
+
+   fprintf(stdout, "%d patterns (%d skipped)\n", pattern_count, skip_count);
+   fprintf(stdout, "%d / %d tests failed\n", fail_count, test_count);
 }
 
 int main(int argc, char **argv) {
